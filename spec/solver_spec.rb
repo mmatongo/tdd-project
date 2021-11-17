deffbb9d47a8da884febf18164f01e5643f0c575
Daniel M. Matongo <m_matongo@icloud.com>
2021-11-17 11:11:34 +0200

    add tests for factorial

---
+++ spec/solver_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'helper_spec'
+
+describe 'Solver' do
+  before :each do
+    @solver = Solver.new
+  end
+
+  describe '#factorial' do
+    context 'when given a number' do
+      it 'return 1 when N == 0' do
+        result = @solver.factorial(0)
+        expect(result).to eq(1)
+      end
+
+      it 'negative numbers' do
+        expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+      end
+
+      it 'returns the factorial of N' do
+        result = @solver.factorial(3)
+        expect(result).to eq(6)
+      end
+    end
+  end
+end
